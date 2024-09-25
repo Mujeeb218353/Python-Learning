e1170140311191d7d9ac08bbbebbba88a3e1bd6c
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2024-09-25 12:59:17 +0500

    Chapter 1

---
+++ Chapter 1/Practice/problem2.py
@@ -0,0 +1,5 @@
+# Use REPL (Read Evaluate Print Loop)
+
+# Write python in terminal(command prompt)
+
+# then print table of 5
---
+++ Chapter 1/Practice/problem3.py
@@ -0,0 +1,6 @@
+import pyttsx3
+
+# use an external python module
+engine = pyttsx3.init()
+engine.say('Twinkle Twinkle little star')
+engine.runAndWait()
