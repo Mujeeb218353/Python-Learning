3667ea890897d8424cd38a039246c3ced4540fda
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-01-22 15:12:56 +0500

    Data types

---
+++ Variables and Data types/data_types.py
@@ -0,0 +1,11 @@
+integer = 10
+float = 1.5
+string = "Hello"
+boolean = True
+NoneType = None
+
+print("Integer:", integer)
+print("Float:", float)
+print("String:", string)
+print("Boolean:", boolean)
+print("NoneType:", NoneType)
