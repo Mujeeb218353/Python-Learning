cd784d3839857b59258dd91ce9375ded2fb8d818
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-02-06 23:07:31 +0500

    String: Problem 2

---
+++ Strings/practice/problem2.py
@@ -0,0 +1,12 @@
+letter = '''Dear <|NAME|>,
+You are selected!
+<|DATE|>
+'''
+
+name = input("Enter your name: ")
+date = input("Enter date: ")
+
+letter = letter.replace("<|NAME|>", name)
+letter = letter.replace("<|DATE|>", date)
+
+print(letter)
