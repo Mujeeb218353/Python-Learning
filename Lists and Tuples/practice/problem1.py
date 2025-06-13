50e4c390508734befab951c6b4b3ceb9e7b4e918
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-06-13 12:43:44 +0500

    Lists and Tuples: Problem 1

---
+++ Lists and Tuples/practice/problem1.py
@@ -0,0 +1,11 @@
+fruits = []
+
+fruits.append(input("Enter fruit 1: "))
+fruits.append(input("Enter fruit 2: "))
+fruits.append(input("Enter fruit 3: "))
+fruits.append(input("Enter fruit 4: "))
+fruits.append(input("Enter fruit 5: "))
+fruits.append(input("Enter fruit 6: "))
+fruits.append(input("Enter fruit 7: "))
+
+print("The fruit list is:", fruits)
