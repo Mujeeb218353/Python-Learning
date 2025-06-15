507eeb6c107fd961731e62dbb803d55727cc4f20
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-06-15 12:24:24 +0500

    Lists and its methods

---
+++ Lists and Tuples/tuples.py
@@ -0,0 +1,9 @@
+empty_tuple = (1,)
+
+print(type(empty_tuple))
+
+fruits = ("apple", "banana", "cherry", "date")
+print(fruits)
+print(fruits[1:3])
+
+# fruits[1] = "blueberry"  # This will raise an error because tuples are immutable
---
+++ Lists and Tuples/tuples_methods.py
@@ -0,0 +1,8 @@
+a = (1, 45, 342, 3424, False, "Hello")
+print(a)
+
+no = a.count(342) # Returns the number of elements with the specified value
+print(no)
+
+index = a.index(False) # Returns the index of the first element with the specified value
+print(index)
