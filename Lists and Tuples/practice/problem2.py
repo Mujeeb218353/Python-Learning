b40f32147140a0c35e924a337fe4a23b85934bab
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-06-17 12:44:04 +0500

    Lists and Tuples: Problem 2

---
+++ Lists and Tuples/practice/problem2.py
@@ -0,0 +1,11 @@
+marks = []
+
+marks.append(int(input("Enter student 1 marks: ")))
+marks.append(int(input("Enter student 2 marks: ")))
+marks.append(int(input("Enter student 3 marks: ")))
+marks.append(int(input("Enter student 4 marks: ")))
+marks.append(int(input("Enter student 5 marks: ")))
+marks.append(int(input("Enter student 6 marks: ")))
+
+marks.sort()
+print("The marks list is:", marks)
