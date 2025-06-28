b494598120f3045b594f0f6b42cc72ba0070cc4b
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-06-28 15:23:43 +0500

    Sets and Dictionaries: Problem 2

---
+++ Sets and Dictionaries/practice/problem2.py
@@ -0,0 +1,9 @@
+s = set()
+
+s.add(int(input("Enter item 1: ")))
+s.add(int(input("Enter item 2: ")))
+s.add(int(input("Enter item 3: ")))
+s.add(int(input("Enter item 4: ")))
+s.add(int(input("Enter item 5: ")))
+
+print("The set contains:", s)
