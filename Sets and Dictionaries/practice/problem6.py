129a684fe6bb8b75c6d22519a29883145baa5c4a
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-07-03 15:25:12 +0500

    Sets and Dictionaries: Problem 6

---
+++ Sets and Dictionaries/practice/problem6.py
@@ -0,0 +1,19 @@
+dictionary = {}
+
+name = input("Enter your name 1: ")
+lang = input("Enter your favorite programming language: ")
+dictionary.update({name: lang})
+
+name = input("Enter your name 2: ")
+lang = input("Enter your favorite programming language: ")
+dictionary.update({name: lang})
+
+name = input("Enter your name 3: ")
+lang = input("Enter your favorite programming language: ")
+dictionary.update({name: lang})
+
+name = input("Enter your name 4: ")
+lang = input("Enter your favorite programming language: ")
+dictionary.update({name: lang})
+
+print(dictionary)
