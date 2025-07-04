d10c5f0dbaf25c0a58ecc81b6c02c60c46bef2eb
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-07-04 15:25:26 +0500

    Sets and Dictionaries: Problem 7

---
+++ Sets and Dictionaries/practice/problem7.py
@@ -0,0 +1,21 @@
+dictionary = {}
+
+name = input("Enter your name 1: ")
+lang = input("Enter your favorite programming language: ")
+dictionary[name] = lang
+
+name = input("Enter your name 2: ")
+lang = input("Enter your favorite programming language: ")
+dictionary[name] = lang
+
+name = input("Enter your name 3: ")
+lang = input("Enter your favorite programming language: ")
+dictionary[name] = lang
+
+name = input("Enter your name 4: ")
+lang = input("Enter your favorite programming language: ")
+dictionary[name] = lang
+
+print(dictionary)
+
+# It's update the previous value if the same name is entered again.
