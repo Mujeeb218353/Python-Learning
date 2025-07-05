f8038920e08965174883c97ea81952cbf505ff4d
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-07-05 15:25:39 +0500

    Sets and Dictionaries: Problem 8

---
+++ Sets and Dictionaries/practice/problem8.py
@@ -0,0 +1,21 @@
+dictionary = {}
+
+name = input("Enter your name 1: ")
+lang = input("Enter your favorite programming language: ")
+dictionary[name] = lang
+
+name = input("Enter your name 2: ")
+lang = input("Enter your favorite programming language: ")
+dictionary[name] = lang
+
+name = input("Enter your name 3: ")
+lang = input("Enter your favorite programming language: ")
+dictionary[name] = lang
+
+name = input("Enter your name 4: ")
+lang = input("Enter your favorite programming language: ")
+dictionary[name] = lang
+
+print(dictionary)
+
+# If language is same again for different names, it will store both.
