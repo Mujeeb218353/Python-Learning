8e950744dee155380533d0656449b45f8c0463ae
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-07-14 23:36:36 +0500

    Conditional Expression: Problem 3

---
+++ Conditional Expression/practice/problem3.py
@@ -0,0 +1,12 @@
+comment = input("Enter your comment: ").lower()
+
+if comment == "make a lot of money":
+    print("This is spam.")
+elif comment == "buy now":
+    print("This is spam.")
+elif comment == "subscribe this":
+    print("This is spam.")
+elif comment == "click this":
+    print("This is spam.")
+else:
+    print("This is not spam.")
