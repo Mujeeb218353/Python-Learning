a674b638c6222272efdae5aec352bd02f152ce60
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-08-08 14:27:12 +0500

    Loops: Problem 7

--- Loops/practice/problem7.py
+++ Loops/practice/problem7.py
@@ -1,21 +1,25 @@
 n = int(input("Enter a number to print pattern: "))
 
 for i in range(1, n+1):
-    for j in range(1, n-i+1):
-        print(" ", end="")
-    for k in range(1, i+1):
-        print("*", end="")
-    for l in range(1, i):
-        print("*", end="")
+    print(" "*(n-i) + "*"*(2*i-1))
     print()
 
-for i in range(1, n+1):
-    line = ""
-    for j in range(n-i):
-        line += " "
-    for k in range(2*i-1):
-        line += "*"
-    print(line)
+# for i in range(1, n+1):
+#     for j in range(1, n-i+1):
+#         print(" ", end="")
+#     for k in range(1, i+1):
+#         print("*", end="")
+#     for l in range(1, i):
+#         print("*", end="")
+#     print()
+
+# for i in range(1, n+1):
+#     line = ""
+#     for j in range(n-i):
+#         line += " "
+#     for k in range(2*i-1):
+#         line += "*"
+#     print(line)
 
 # '''
 #    *      i=1, 5 space, 1 star
