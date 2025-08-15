a0f0ea552e9b7ae852ca1af2b1da6c17105c902e
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-08-15 15:40:59 +0500

    Functions and Recursions: Problem 6

---
+++ Functions and Recursions/practice/problem6.py
@@ -0,0 +1,11 @@
+def inches_to_cms(inches):
+    cm = inches*2.54
+    return cm
+
+print(inches_to_cms(5))
+
+def cms_to_inches(cms):
+    inches = cms/2.54
+    return inches
+
+print(cms_to_inches(5))
