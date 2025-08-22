1b7da9eda2dcf361bac7f9391f4de9633bf75e70
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-08-22 12:05:50 +0500

    OOP: Class

---
+++ Intro to OOP/class.py
@@ -0,0 +1,11 @@
+class Employee:
+    language = "Python"
+    salary = 1200000  # This is class attribute
+
+employee1 = Employee()
+
+employee1.name = "Mujeeb Ur Rahman" # This an object/instance attribute
+employee1.age = 22
+employee1.contact = "03455109786"
+
+print(employee1.name, employee1.age, employee1.contact, employee1.language, employee1.salary)
