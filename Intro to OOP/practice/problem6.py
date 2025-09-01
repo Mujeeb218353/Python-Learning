2731d0d4f0f65868703a9f45ad754ebc30e31d54
Mujeeb Ur Rehman <mujeeb218353@gmail.com>
2025-09-01 12:08:38 +0500

    OOP: Problem 6

---
+++ Intro to OOP/practice/problem6.py
@@ -0,0 +1,20 @@
+from random import randint
+class Train:
+
+    def __init__(slf, train_no):
+        slf.train_no = train_no
+
+    def book_ticket(self, fro, to):
+        print(f"Ticket is booked in train no: {self.train_no} from {fro} to {to}")
+
+    def get_status(self, fro, to):
+        print(f"Train no: {self.train_no} from {fro} to {to} is running...")
+
+    def get_fare(self, fro, to):
+        print(f"Ticket fare in train no: {self.train_no} from {fro} to {to} is {randint(1000, 5000)}")
+
+
+t1 = Train(123)
+t1.book_ticket("Karachi", "Rohri")
+t1.get_status("Karachi", "Rohri")
+t1.get_fare("Karachi", "Rohri")
